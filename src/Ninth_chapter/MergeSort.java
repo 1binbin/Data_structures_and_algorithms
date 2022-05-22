e4105e5b96b16c0c6d55eadcae5992b492a3ca5b
hongxiaobin <2812181610@qq.com>
2022-05-22 11:50:11 +0800

    2812181610@qq.com

---
+++ src/Ninth_chapter/MergeSort.java
@@ -0,0 +1,26 @@
+package Ninth_chapter;
+
+/**
+ * @Author hongxiaobin
+ * @Time 2022/5/22-11:44
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 5, 7, 1, 3, 6, 2};
+    }
+
+    /**
+     * 合并的方法
+     *
+     * @Param: [arr 待排序数组, left 左边有序序列初始索引, mid 中间索引, right 右边索引, temp 暂存数组]
+     * @Return:
+     */
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+//        初始化左右两边有序序列的索引
+        int i = left;
+        int j = mid + 1;
+//        初始化临时数组的索引
+        int t = 0;
+
+    }
+}
