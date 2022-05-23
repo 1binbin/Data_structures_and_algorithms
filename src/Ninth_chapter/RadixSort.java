82f07b1c18c59eab96a17beedde31f603653e40f
hongxiaobin <2812181610@qq.com>
2022-05-23 09:22:51 +0800

    2812181610@qq.com

--- src/Ninth_chapter/RadixSort.java
+++ src/Ninth_chapter/RadixSort.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
  */
 public class RadixSort {
     public static void main(String[] args) {
-        int n = 20000000;
+        int n = 80000000;
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 10000);
