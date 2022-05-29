6bc4936d118e7e4eddd6e4f43e273bf510a7498e
hongxiaobin <2812181610@qq.com>
2022-05-29 17:47:35 +0800

    2812181610@qq.com

---
+++ src/Tenth_chapter/GreedyAlgorithm.java
@@ -0,0 +1,86 @@
+package Tenth_chapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 贪心算法-最大覆盖集合
+ *
+ * @Author hongxiaobin
+ * @Time 2022/5/29-17:22
+ */
+public class GreedyAlgorithm {
+    public static void main(String[] args) {
+//        创建广播电台
+        HashMap<String, HashSet<String>> broadcasts = new HashMap<>();
+        HashSet<String> hashSet1 = new HashSet<>();
+        hashSet1.add("北京");
+        hashSet1.add("上海");
+        hashSet1.add("天津");
+        HashSet<String> hashSet2 = new HashSet<>();
+        hashSet2.add("广州");
+        hashSet2.add("北京");
+        hashSet2.add("深圳");
+        HashSet<String> hashSet3 = new HashSet<>();
+        hashSet3.add("成都");
+        hashSet3.add("上海");
+        hashSet3.add("杭州");
+        HashSet<String> hashSet4 = new HashSet<>();
+        hashSet4.add("上海");
+        hashSet4.add("天津");
+        HashSet<String> hashSet5 = new HashSet<>();
+        hashSet5.add("杭州");
+        hashSet5.add("大连");
+        broadcasts.put("K1", hashSet1);
+        broadcasts.put("K2", hashSet2);
+        broadcasts.put("K3", hashSet3);
+        broadcasts.put("K4", hashSet4);
+        broadcasts.put("K5", hashSet5);
+
+//        存放所有地区
+        HashSet<String> allAreas = new HashSet<>();
+        allAreas.add("北京");
+        allAreas.add("上海");
+        allAreas.add("天津");
+        allAreas.add("广州");
+        allAreas.add("深圳");
+        allAreas.add("成都");
+        allAreas.add("杭州");
+        allAreas.add("大连");
+//        存放选择的电台集合
+        ArrayList<String> selects = new ArrayList<>();
+//        定义一个临时的集合，在遍历过程中存放遍历过程中的电台覆盖的地区和当前还没有覆盖的地区的交集
+//        就是还有几个可以覆盖的
+        HashSet<String> tempSet = new HashSet<>();
+//        定义一个变量，保存在一次遍历过程中能够覆盖最多未覆盖的地区对应的电台的key
+//        如果变量不为空，则加入到selects中
+        String maxKey;
+//        如果allAreas不为0，则表示还没有覆盖的地区
+        while (allAreas.size() != 0) {
+//            没进行一次while都要把maxKey置空
+            maxKey = null;
+            for (String key : broadcasts.keySet()) {
+//                把tempSet清空
+                tempSet.clear();
+//                当前的key能覆盖的地区
+                HashSet<String> areas = broadcasts.get(key);
+                tempSet.addAll(areas);
+//                求和tempSet和allAreas集合的交集，交集覆盖tempSet
+                tempSet.retainAll(allAreas);
+//                如果当前的集合包含了未覆盖地区的数量，比maxKey指向的集合未覆盖的地区还多，就需要重置maxKey
+                if (tempSet.size() > 0 && (maxKey == null || tempSet.size() > broadcasts.get(maxKey).size())) {
+                    maxKey = key;
+                }
+            }
+            if (maxKey != null) {
+                selects.add(maxKey);
+//                清除所有地区中已覆盖地区
+                allAreas.removeAll(broadcasts.get(maxKey));
+            }
+        }
+
+//        输出结果
+        System.out.println(selects);
+    }
+}
--- src/Tenth_chapter/KMPAlgorithm.java
+++ src/Tenth_chapter/KMPAlgorithm.java
@@ -1,5 +1,7 @@
 package Tenth_chapter;
 
+import java.util.Arrays;
+
 /**
  * KMP算法
  *
@@ -8,10 +10,58 @@ package Tenth_chapter;
  */
 public class KMPAlgorithm {
     public static void main(String[] args) {
-
+        String str1 = "BBC ABCDAB ABCDABCDABDE";
+        String str2 = "ABCDABD";
+        int[] next = kmpNext(str2);
+        System.out.println(Arrays.toString(next));
+        System.out.println(kmpSearch(str1, str2, next));
     }
-    /** 实现KMP算法
-     * @Param:
-     * @Return:
+
+    /**
+     * KMP搜索算法
+     *
+     * @Param: String str1 目标字符串  String str2 子串  int[] next 子串对应的部分匹配表
+     * @Return: 如果找到返回第一个匹配的位置，否则返回-1
      */
+    public static int kmpSearch(String str1, String str2, int[] next) {
+        for (int i = 0, j = 0; i < str1.length(); i++) {
+//            核心！需要处理不想等的情况,去调整j的大小
+            while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (str1.charAt(i) == str2.charAt(j)) {
+                j++;
+            }
+            if (j == str2.length()) {
+                return i - j + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 获取部分匹配表
+     *
+     * @Param: String dest 字符串
+     * @Return: int[] 匹配表
+     */
+    public static int[] kmpNext(String dest) {
+//        创建一个数组，保存部分匹配值
+        int[] next = new int[dest.length()];
+//        如果dest的长度为1，它的部分匹配值为0
+        next[0] = 0;
+        for (int i = 1, j = 0; i < dest.length(); i++) {
+//            当dest.charAt(i) != dest.charAt(j)需要从next[j-1]获取新的j
+//            知道发现有dest.charAt(i) == dest.charAt(j)成立才退出
+            while (j > 0 && dest.charAt(i) != dest.charAt(j)) {
+                j = next[j - 1];
+            }
+//            当dest.charAt(i) == dest.charAt(j)这个条件满足时，部分匹配值就是要加1
+            if (dest.charAt(i) == dest.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
+    }
 }
