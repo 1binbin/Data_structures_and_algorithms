d53edb5207f579b66912937c5b64028ecf61290f
hongxiaobin <2812181610@qq.com>
2022-06-10 22:26:00 +0800

    2812181610@qq.com

---
+++ src/Tenth_chapter/PrimAlgorithm.java
@@ -0,0 +1,15 @@
+package Tenth_chapter;
+
+/**
+ * @Author hongxiaobin
+ * @Time 2022/6/10-22:20
+ */
+public class PrimAlgorithm {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Mraph {
+
+}
